67813e8ff62bea41f793ee01f5a33698e4a40f06
Dieggo775 <dieggo_taylor@hotmail.com>
2026-05-06 14:15:44 -0300

    modified

--- Projeto_01/app.js
+++ Projeto_01/app.js
@@ -12,3 +12,26 @@
         BODY-PARSER - npm install body-parser --save
 */
 
+// Import das dependencias
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Criando um objeto do tipo express
+const app = express();
+
+//request => Chegara na API
+//response => Retorno da API
+
+//API Privadas => APIs internas (particulares) das empresas
+//API Publicas => APIs externas, geralmente livres para utilizacao
+
+app.use((request, response, next) => {
+    // Access-Control-Allow-Origin configuracao para quem poderar acessar a API
+    response.header('Access-Control-Allow-Origin', '*');
+    //Access-Control-Allow-Method serve para escolher que tipo de requisicao a API recebera
+    response.header('Access-Control-Allow-Methods', 'GET');
+
+    app.use(cors());
+    next(); //para continuar para a proxima etapa
+})
