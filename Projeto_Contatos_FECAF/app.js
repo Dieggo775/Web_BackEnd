a7363df13015cf9730536f3499baba6de0a110ae
Dieggo775 <dieggo_taylor@hotmail.com>
2026-05-11 21:48:24 -0300

    Integração Prisma

--- Projeto_Contatos_FECAF/app.js
+++ Projeto_Contatos_FECAF/app.js
@@ -1,8 +1,24 @@
-// MVC - (model, view, controller)
 /*
-
+    // MVC - (model, view, controller)
     Model - Modelagem dos dados (BD)
     Controller - É responsavel pela regra de negócio do projeto
     View - É responsavel pela interação com o cliente (usuário)
 
+    Dependencias para acesso ao Banco de Dados (ORMS)
+    - Sequelize
+    - Prisma*
+    - Fastfy
+
+    Instalação e configuração do prisma
+        npm install prisma --save
+        npx prisma init
+        npm install @prisma/client --save
 */
+
+/*
+* Objetivo: API para realizar requisições de contatos da UniFecaf
+* Data: 11/05/2026
+* Autor: Diego Araujo
+* Versão: 1.0
+*/
+
