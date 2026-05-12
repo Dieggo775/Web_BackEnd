cc59e60447596299b38c27edacada094eab7fb05
Dieggo775 <dieggo_taylor@hotmail.com>
2026-05-11 21:09:21 -0300

    Novo Projeto

---
+++ Projeto_Contatos_FECAF/app.js
@@ -0,0 +1,8 @@
+// MVC - (model, view, controller)
+/*
+
+    Model - Modelagem dos dados (BD)
+    Controller - É responsavel pela regra de negócio do projeto
+    View - É responsavel pela interação com o cliente (usuário)
+
+*/
